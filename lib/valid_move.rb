0aa0bfff9a8751eab0eaad60da510437ef12b929
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-05 20:31:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,9 +8,7 @@ end
 taken = nil
 def position_taken?(board, index)
 if board[index] == "  " || board[index] == " " || board[index] == nil
-  taken = false
-else
-  taken = true
+  false
 end
 end
 taken
